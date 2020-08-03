ce900da2093391ff56e06dba7151ef9b66b4aec0
zfan <13973123764@163.com>
2020-08-03 20:41:54 +0800

    first commit

---
+++ src/api/login.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+const reqUrl = 'http://127.0.0.1'
+
+export function login(username, password) {
+  return request({
+    url: '/admin/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/admin/info',
+    method: 'get',
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/admin/logout',
+    method: 'post'
+  })
+}
